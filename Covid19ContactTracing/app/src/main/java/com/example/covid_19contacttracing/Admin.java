67ccdf1564e28ff38eee6fcd0f9d8e39c29748c3
winson <weixiong8489@gmail.com>
2021-01-07 11:56:00 +0800

    added template for admin shop list

---
+++ Covid19ContactTracing/app/src/main/java/com/example/covid_19contacttracing/Admin.java
@@ -0,0 +1,53 @@
+package com.example.covid_19contacttracing;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.zxing.integration.android.IntentResult;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Admin extends User {
+    private CustStatus status;
+    private ArrayList<HashMap<String, String>> history;
+
+    static FirebaseAuth fAuth;
+    static FirebaseFirestore fStore;
+
+    public Admin()
+    {
+        // Initializing Firebase
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+    }
+
+    // flag customers with id given
+    public void flagCustomer()
+    { }
+
+    // generate QR code
+    public void generateQR()
+    { }
+
+    // Opens shop list page
+    public void showShopList()
+    { }
+
+    // Opens master history page
+    public void showMasterHistory()
+    { }
+}
---
+++ Covid19ContactTracing/app/src/main/java/com/example/covid_19contacttracing/AdminShopProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.covid_19contacttracing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminShopProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_shop_profile);
+    }
+}
