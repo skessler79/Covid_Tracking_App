80d8ac17e95eaa7006a4392612d672ec9b00e6f3
skessler79 <selwyn.kessler79@gmail.com>
2021-01-10 21:59:48 +0800

    removed landscape and dark mode

--- Covid19ContactTracing/app/src/main/java/com/example/covid_19contacttracing/IntroductoryActivity.java
+++ Covid19ContactTracing/app/src/main/java/com/example/covid_19contacttracing/IntroductoryActivity.java
@@ -1,6 +1,7 @@
 package com.example.covid_19contacttracing;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -31,6 +32,7 @@ public class IntroductoryActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introductory);
 
--- Covid19ContactTracing/app/src/main/java/com/example/covid_19contacttracing/SendOTPActivity.java
+++ Covid19ContactTracing/app/src/main/java/com/example/covid_19contacttracing/SendOTPActivity.java
@@ -2,6 +2,7 @@ package com.example.covid_19contacttracing;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -24,6 +25,7 @@ public class SendOTPActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_o_t_p);
 
